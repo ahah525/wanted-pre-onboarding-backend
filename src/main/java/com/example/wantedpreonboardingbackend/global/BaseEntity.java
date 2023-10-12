80dac15af51ef7108942e864c561c9747dd6a966
ahah525 <ahah525@naver.com>
2023-10-12 16:30:12 +0900

    feat : BaseEntity 추가

---
+++ src/main/java/com/example/wantedpreonboardingbackend/global/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.example.wantedpreonboardingbackend.global;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+}
